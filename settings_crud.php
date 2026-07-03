7a11e6cfca7bb8ee65ea87f32398053e9d9cf0d0
Harshita Sharma <harshitasharma84458@gmail.com>
2026-07-03 18:04:42 +0530

    Add files via upload

---
+++ settings_crud.php
@@ -0,0 +1,14 @@
+<?php
+    require('../essentials.php');
+    require('../db_config.php');
+    adminLogin(); 
+
+    if(isset($_POST['get_general'])){
+        $q = "SELECT * FROM `settings`WHERE `sr_no`=?";
+        $values = [1];
+        $res = select($q,$values,"i");
+        $data = mysqli_fetch_assoc($res);
+        $json_data = json_encode($data);
+        echo $json_data;
+    }
+?>
